3657d7ded5848989bc149cf1990c5720abd166c2
FRANCISCO GARRIDO MOSQUEIRA <garrido.mosqueira@gmail.com>
2017-10-18 23:01:35 +0200

    Application yml for Spring Cloud config repository

--- Udemy/SpringCloud-Labs/lab-4-sentence/src/main/java/com/springcloud/lab4/sentence/Lab4SentenceApplication.java
+++ Udemy/SpringCloud-Labs/lab-4-sentence/src/main/java/com/springcloud/lab4/sentence/Lab4SentenceApplication.java
@@ -3,6 +3,9 @@ package com.springcloud.lab4.verb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,5 +13,13 @@ public class Lab4SentenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab4SentenceApplication.class, args);
+	}
+
+	//	This "LoadBalanced" RestTemplate
+	//	is automatically hooked into Ribbon:
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- Udemy/SpringCloud-Labs/lab-4-sentence/src/main/java/com/springcloud/lab4/sentence/action/SentenceController.java
+++ Udemy/SpringCloud-Labs/lab-4-sentence/src/main/java/com/springcloud/lab4/sentence/action/SentenceController.java
@@ -15,22 +15,70 @@ import java.util.List;
 @RestController
 public class SentenceController {
 
+    //	This is referencing the RestTemplate we defined earlier:
+    @Autowired
+    RestTemplate template;
+
     @Autowired
     DiscoveryClient client;
 
+    /**
+     * Display a small list of Sentences to the caller:
+     */
     @GetMapping("/sentence")
     public @ResponseBody
     String getSentence() {
         return
+            "<h3>Some Sentences</h3><br/>" +
+                    buildSentence() + "<br/><br/>" +
+                    buildSentence() + "<br/><br/>" +
+                    buildSentence() + "<br/><br/>" +
+                    buildSentence() + "<br/><br/>" +
+                    buildSentence() + "<br/><br/>"
+            ;
+    }
+
+    @GetMapping("/sentence-differ")
+    public @ResponseBody
+    String getSentenceDiffer() {
+        return
                 getWord("LAB-4-SUBJECT") + " "
-                        + getWord("LAB-4-VERB") + " "
-                        + getWord("LAB-4-ARTICLE") + " "
-                        + getWord("LAB-4-ADJECTIVE") + " "
-                        + getWord("LAB-4-NOUN") + "."
+                        + getWord2("LAB-4-VERB") + " "
+                        + getWord2("LAB-4-ARTICLE") + " "
+                        + getWord2("LAB-4-ADJECTIVE") + " "
+                        + getWord2("LAB-4-NOUN") + "."
                 ;
     }
 
+    /**
+     * Assemble a sentence by gathering random words of each part of speech:
+     */
+    public String buildSentence() {
+        String sentence = "There was a problem assembling the sentence!";
+        try {
+            sentence =
+                String.format("%s %s %s %s %s.",
+                        getWord("SUBJECT"),
+                        getWord("VERB"),
+                        getWord("ARTICLE"),
+                        getWord("ADJECTIVE"),
+                        getWord("NOUN"));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return sentence;
+    }
+
+    /**
+     * Obtain a random word for a given part of speech, where the part
+     * of speech is indicated by the given service / client ID:
+     */
     public String getWord(String service) {
+        return template.getForObject("http://" + service, String.class);
+
+    }
+
+    public String getWord2(String service) {
         List<ServiceInstance> list = client.getInstances(service);
         if (list != null && list.size() > 0) {
             URI uri = list.get(0).getUri();
